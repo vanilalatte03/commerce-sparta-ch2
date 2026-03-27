52bbae3df2df52d90254cb428b99da5eca56cbd4
JH <jiho4890@gmail.com>
2026-03-27 17:38:25 +0900

    feat: 장바구니 출력 및 금액 계산 구현

--- src/com/jiho/commerce/CommerceSystem.java
+++ src/com/jiho/commerce/CommerceSystem.java
@@ -20,26 +20,53 @@ public class CommerceSystem {
                 System.out.println((i + 1) + ". " + categories.get(i).getCategoryName());
             }
             System.out.println("0. 종료      | 프로그램 종료");
+
+            if(!shoppingBaskets.isEmpty()){
+                System.out.println("\n[ 주문 관리 ]\n" +
+                        "4. 장바구니 확인      | 장바구니를 확인 후 주문합니다.\n" +
+                        "5. 주문 취소       | 진행중인 주문을 취소합니다.");
+            }
+
             int chooseCategory= sc.nextInt();
+
+            //장바구니 확인
+            if (!shoppingBaskets.isEmpty() && chooseCategory == 4) {
+                System.out.println("아래와 같이 주문 하시겠습니까?\n");
+                System.out.println("[ 장바구니 내역 ]");
+                int sumPrice = 0;
+                for (int i = 0; i < shoppingBaskets.size(); i++) {
+                    System.out.printf("%d. %-11s | %,10d원 | 수량: %s개\n",
+                            i + 1,
+                            shoppingBaskets.get(i).getProductName(),
+                            shoppingBaskets.get(i).getPriceInformation(),
+                            shoppingBaskets.get(i).getQuantity());
+                    sumPrice += shoppingBaskets.get(i).getPriceInformation();
+                }
+                System.out.println("[ 총 주문 금액 ]");
+                System.out.printf("%,10d원\n\n", sumPrice);
+
+                continue;
+            }
+
+            //주문 취소
+            if (!shoppingBaskets.isEmpty() && chooseCategory == 5) {
+                System.out.println("주문 취소");
+                shoppingBaskets.clear();
+                continue;
+            }
 
             if (chooseCategory == 0) {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 return;
-            } else if (chooseCategory < 0 || chooseCategory > categories.size()){
+            } else if (chooseCategory < 0 || chooseCategory > categories.size()){ //예외
                 System.out.println("유효하지 않은 카테고리 번호입니다.");
                 continue;
             }
 
-
-
             Category category = categories.get(chooseCategory - 1);
 
             System.out.println();
-            try {
-                System.out.println("[ " + category.getCategoryName() + " 카테고리 ]");
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(e.getMessage());
-            }
+            System.out.println("[ " + category.getCategoryName() + " 카테고리 ]");
 
             for (int i = 0; i < category.getProducts().size(); i++) {
                 Product product = category.getProducts().get(i);
@@ -56,7 +83,7 @@ public class CommerceSystem {
             if (chooseProduct == 0) {
                 System.out.println();
                 continue;
-            } else if (chooseProduct < 0|| chooseProduct > category.getProducts().size()) {
+            } else if (chooseProduct < 0|| chooseProduct > category.getProducts().size()) {//예외
                 System.out.println("유효하지 않은 상품 번호입니다.");
                 continue;
             }
@@ -78,8 +105,6 @@ public class CommerceSystem {
                     ShoppingBasket shoppingBasket = new ShoppingBasket(product.getProductName(), 1, product.getPrice());
                     shoppingBaskets.add(shoppingBasket);
                     System.out.println(product.getProductName() + "가 장바구니에 추가되었습니다.");
-                    product.setStock(product.getStock()-1);
-                    System.out.println("남은 재고: " + product.getStock());
                 }
             }
 
